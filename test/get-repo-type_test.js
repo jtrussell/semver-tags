fb14d71c11cedeb5dc75c0cf6c13a5936c31955b
Justin <jrussell@ivantagehealth.com>
2013-09-06 08:45:09 -0400

    feature: Auto detect repo type
    I.e. git vs svn.
    
    TODO: This uses `findup-sync` need to make async

--- test/get-repo-type_test.js
+++ test/get-repo-type_test.js
@@ -1,24 +1,34 @@
-/*global describe, it */
+/*global describe, afterEach, it */
 'use strict';
 
 var get_repo_type = require('../lib/get-repo-type.js')
-  , expect = require('chai').expect;
+  , expect = require('chai').expect
+  , exec = require('child_process').exec;
 
 /*
  * http://chaijs.com/api/bdd/
  */
 
 describe('get-repo-type', function() {
+  var origDir = process.cwd();
+
+  afterEach(function() {
+    process.chdir(origDir);
+  });
 
   it('should recognize git repos', function(done) {
-    get_repo_type(__dirname + '/fixtures/git-dir', function(err, type) {
+    var gitDir = __dirname + '/fixtures/git-dir'; // execute with gitDir as cwd
+    process.chdir(gitDir);
+    get_repo_type(function(err, type) {
       expect(type).to.equal('git');
       done();
     });
   });
 
   it('should recognized svn repos', function(done) {
-    get_repo_type(__dirname + '/fixtures/svn-dir', function(err, type) {
+    var svnDir = __dirname + '/fixtures/svn-dir';
+    process.chdir(svnDir);
+    get_repo_type(function(err, type) {
       expect(type).to.equal('svn');
       done();
     });
