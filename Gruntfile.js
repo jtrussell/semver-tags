2465b4a173837d4cd2deff375d45bd4e9a307924
Justin <jrussell@ivantagehealth.com>
2013-09-06 09:21:58 -0400

    test: Creat dummy git dir automatically
    Because git doesn't want to check it in

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,11 +36,16 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask('addGitDirForTest', function() {
+    // Kinda gross but git won't check this in
+    grunt.file.write(__dirname + '/test/fixtures/git-dir/.git/foo', '');
+  });
+
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-simple-mocha');
 
-  grunt.registerTask('test', ['jshint', 'simplemocha']);
+  grunt.registerTask('test', ['jshint', 'addGitDirForTest', 'simplemocha']);
 
   grunt.registerTask('default', ['test']);
 
